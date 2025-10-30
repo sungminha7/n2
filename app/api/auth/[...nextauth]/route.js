4fc0a72434e7094e0ea8eb117fd15c1b09f23575
HaSungMin <devilone@naver.com>
2025-10-30 18:19:36 +0900

    31 JWT AUTH

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,10 +13,60 @@ export const authOptions = {
       async authorize(credentials, req) {
         console.log("---------------------------------");
         console.log("credentials: ", credentials);
+
+        const res = await fetch(`http://localhost:8080/api/accounts/signin`, {
+          method: "POST",
+          body: JSON.stringify({
+            username: credentials.username,
+            password: credentials.password,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+        console.log("---------------------------------");
+        console.log("res: ", res);
+
+        const user = await res.json();
+        console.log("---------------------------------");
+        console.log("user: ", user);
+
+        if (res.ok && user) {
+          return user;
+        }
         return null;
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      console.log("jwt params : jwt--------------------");
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.email = user.email;
+        token.name = user.nickname;
+
+        token.accessToken = user.accessToken;
+        token.refreshToken = user.refreshToken;
+        token.expiresAt = Date.now() + 1000 * 60 * 60;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      console.log("session--------------------");
+      session.user.id = token.id;
+      session.user.role = token.role;
+      session.user.email = token.email;
+      session.user.name = token.nickname;
+
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.expiresAt = Date.now() + 1000 * 60 * 60;
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
